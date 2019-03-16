ed47e92949af7aa9924a22cfbb8f44d935b97fa6
咸菜 <lplee7830@gmail.com>
2019-03-16 16:34:48 +0800

    add eslint and start write backend code ,first step,get data from data base

--- lianjia/src/index.js
+++ lianjia/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './view/style/index.css';
+import App from './view/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+(async () => {
+    // create app
+    /**
+     * @param {*} Component
+     */
+    const render = Component => {
+        ReactDOM.render(
+            <Component />,
+            document.getElementById('root')
+        );
+    };
 
+    render(App);
+})();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
