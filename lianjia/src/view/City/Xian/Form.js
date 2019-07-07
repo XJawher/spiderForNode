a94a5aa4ec9b68852a592b4a7d0b91c59c384de0
lipc <lipc@lipcdeMacBook-Pro.local>
2019-07-07 11:52:41 +0800

    modify new data structure

--- lianjia/src/view/City/Xian/Form.js
+++ lianjia/src/view/City/Xian/Form.js
@@ -20,7 +20,7 @@ export default function FormComponent (props){
     }
 
     const {xian} = useMappedState(mapState);
-    if (xian.length === 1110) {
+    if (xian.length === 10000) {
         /**
          *
             "2019-4-21":
@@ -30,9 +30,11 @@ export default function FormComponent (props){
             "2019-5-19":
             "2019-5-26":
             "2019-5-30":[]
-            const date = ["2019-4-21","2019-4-28","2019-5-5","2019-5-13","2019-5-26","2019-5-30","2019-6-6","2019-6-14"]
+            const date = ["2019-4-21","2019-4-28","2019-5-5",
+            "2019-5-13","2019-5-26","2019-5-30","2019-6-6",
+            "2019-6-14","2019-6-20","2019-6-30","2019-7-4","2019-7-7"]
          */
-        const date = ["2019-6-14"];
+        const date = ["2019-7-7"];
         date.forEach(item => http.getCityByCondition('', item));
     }
 
