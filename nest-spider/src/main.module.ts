0a758b857e7b146a469a99107dcb440da2700dff
咸菜 <lplee7830@gmail.com>
2021-07-12 21:53:10 +0800

    delete no used code and file

--- nest-spider/src/main.module.ts
+++ nest-spider/src/main.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { CitiesController } from './controllers/cities/cities.controller';
-import { AppService } from './app.service';
 
 import { APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from './pipe/validation.pipe';
 // 新增管道
 @Module({
   imports: [],
-  controllers: [AppController, CitiesController],
-  providers: [{ provide: APP_PIPE, useClass: ValidationPipe }, AppService],
+  controllers: [CitiesController],
+  providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
-export class AppModule {}
+export class MainModule {}
