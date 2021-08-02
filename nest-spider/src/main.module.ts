f73676760a733adc1072b99cf79938233c30d5c9
咸菜 <lplee7830@gmail.com>
2021-08-02 16:23:03 +0800

    MongoDB of cats

--- nest-spider/src/main.module.ts
+++ nest-spider/src/main.module.ts
@@ -4,12 +4,13 @@ import { APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from './pipe/validation.pipe';
 import { DailyNewsController } from './features/daily-news/daily-news.controller';
 import { DailyNewsService } from './features/daily-news/daily-news.service';
+import { CatsModule } from './features/cats/cats.module';
 
 // MongoDB 使用方法 https://docs.nestjs.cn/8/techniques?id=mongo
 import { MongooseModule } from '@nestjs/mongoose';
 // 新增管道
 @Module({
-  imports: [HttpModule, MongooseModule.forRoot("mongodb://localhost/nest")],
+  imports: [HttpModule, MongooseModule.forRoot("mongodb://localhost/nest"), CatsModule],
   controllers: [DailyNewsController],
   providers: [{ provide: APP_PIPE, useClass: ValidationPipe }, DailyNewsService],
 })
